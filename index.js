1212da0664712f1b67c5f5ad330332507be9de54
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-05-04 19:40:58 +0000

    added scrolltop

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+const scrollTop = window.pageYOffset ||
+    ( document.documentElement && document.documentElement.scrollTop ) ||
+    document.body.scrollTop;
+
 const scrollToTop = () => window.scrollTo(0, 0);
 
 export { scrollToTop };
