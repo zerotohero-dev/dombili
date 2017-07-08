d7528d50a92dbb49830aa397a308d0f312f64d46
Volkan Ozcelik <me@volkan.io>
2017-07-08 00:11:21 -0700

    minor changes

--- src/create.js
+++ src/create.js
@@ -8,7 +8,7 @@
  *  _/\_/\_/\_,  ,_/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
  *            )  )   This project is a part of the
  *           (  (   “Byte-Sized JavaScript” videocasts.
- *           )  )  Watch “Byte-Sized JavaScript” at: https://bytesized.tv
+ *           )  )  Watch “Byte-Sized JavaScript” at: https://bytesized.tv/
  *          (__(  MIT licensed — See LICENSE.md
  *               Send your comments, suggestions, and feedback to me@volkan.io
  */
--- src/scroll.js
+++ src/scroll.js
@@ -8,11 +8,10 @@
  *  _/\_/\_/\_,  ,_/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
  *            )  )   This project is a part of the
  *           (  (   “Byte-Sized JavaScript” videocasts.
- *           )  )  Watch “Byte-Sized JavaScript” at: https://bytesized.tv
+ *           )  )  Watch “Byte-Sized JavaScript” at: https://bytesized.tv/
  *          (__(  MIT licensed — See LICENSE.md
  *               Send your comments, suggestions, and feedback to me@volkan.io
  */
-
 /**
  * Gets the left scroll offset of the window in pixels.
  *
